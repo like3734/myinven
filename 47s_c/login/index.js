967548c656ed52f0b92c5e2d6e05836e57ab9f8f
seojongin <like3734>
2018-03-15 12:11:51 +0900

    remove webstorm .idea directory

--- 47s_c/login/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var login = require('./login');
-var check = require('./check');
-
-router.use('/login', login);
-router.use('/check', check);
-
-module.exports = router;
